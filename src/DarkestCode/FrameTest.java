2446de5dccb6631a004da9d27452c93831c63cea
metam <metam@DESKTOP-580PBF1>
2025-04-08 21:47:11 +0200

    Interface finie!

--- src/DarkestCode/FrameTest.java
+++ src/DarkestCode/FrameTest.java
@@ -8,12 +8,14 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class FrameTest extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,6 +37,8 @@ public class FrameTest extends JFrame {
 	 * Create the frame.
 	 */
 	public FrameTest() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(FrameTest.class.getResource("/DarkestCode/Ressources/torch.png")));
+		JLabel label = new JLabel(new ImageIcon("/DarkestCode/Ressources/torch.png"));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -44,18 +48,26 @@ public class FrameTest extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Hello");
-		btnNewButton.setBounds(11, 229, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		textField = new JTextField();
-		textField.setBounds(110, 230, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("D:\\eclipse-workspace\\UrbanMarginal\\media\\dark.jpg"));
+		lblNewLabel.setIcon(new ImageIcon(FrameTest.class.getResource("/DarkestCode/Ressources/dark.jpg")));
 		lblNewLabel.setBounds(0, 0, 434, 261);
 		contentPane.add(lblNewLabel);
+		
+		JLabel title = new JLabel("");
+		title.setIcon(new ImageIcon(FrameTest.class.getResource("/DarkestCode/Ressources/Darkest_Dungeon_Logo.png")));
+		title.setBounds(0, 0, 200, 76);
+		contentPane.add(title);
+		contentPane.setComponentZOrder(title, 0);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"The Hamlet", "The ruins", "Cove battle"}));
+		comboBox.setBounds(10, 191, 81, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnPlay = new JButton("Play");
+		btnPlay.setBounds(10, 224, 81, 23);
+		contentPane.add(btnPlay);
+		contentPane.setComponentZOrder(btnPlay, 0);
+		
 	}
 }
